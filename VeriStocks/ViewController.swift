741718572708dd4466e4e97e0da2b1912ea43832
Olgu Sirman <olgusirman@pudra.com>
2017-08-08 19:04:49 +0300

    Initial Commit

---
+++ VeriStocks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VeriStocks
+//
+//  Created by Olgu Sirman on 08/08/2017.
+//  Copyright © 2017 Olgu Sirman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
